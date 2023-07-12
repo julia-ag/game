d78bd856193c12b5c4aae160d8f3078808d229b1
guega14 <131879091+julia-ag@users.noreply.github.com>
2023-07-12 10:17:08 -0300

    Adicionando box colider

--- game julia/Assets/Script/Player.cs
+++ game julia/Assets/Script/Player.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Player : MonoBehaviour
 {
+    //variavel para o rigidBody
+    private Rigidbody2D rig;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>();  
     }
 
     // Update is called once per frame
